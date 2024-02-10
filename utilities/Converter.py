ab731b36993cc1121a9723ec5141296ff3ecf538
michalszubinski <mich999.pl@gmail.com>
2024-02-10 10:13:29 +0100

    Fixed a bug in Converter (linear to DBFS) - now properly divides by 2^(n-1); Added a way to convert DBFS to linear value

--- utilities/Converter.py
+++ utilities/Converter.py
@@ -9,7 +9,7 @@ class Converter:
 
     @staticmethod
     def linear_to_dbfs_bits(value,bit_depth):
-        valueDBFS = 20 * math.log10(abs(value)/bit_depth)
+        valueDBFS = 20 * math.log10(abs(value)/pow(2,bit_depth-1))
         sign = 1
         if value < 0:
             sign = -1
@@ -19,3 +19,13 @@ class Converter:
     @staticmethod
     def linear_to_dbfs(value):
         return Converter.linear_to_dbfs_bits(value,1)
+
+    @staticmethod
+    def dbfs_to_linear(dbfs):
+        return Converter.dbfs_to_linear_bits(dbfs, 1)
+
+    @staticmethod
+    def dbfs_to_linear_bits(dbfs, bit_depth):
+        linear_value = pow(10, dbfs.DBFS/20)*pow(2,bit_depth-1)
+        linear_value *= dbfs.sign
+        return linear_value
