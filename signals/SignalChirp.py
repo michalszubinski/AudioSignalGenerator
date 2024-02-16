401adb9bbe90ff80bd4bbe2220fddfdea02c5c9f
michalszubinski <mich999.pl@gmail.com>
2024-02-16 12:18:07 +0100

    Added support for linear frequency change

--- signals/SignalChirp.py
+++ signals/SignalChirp.py
@@ -1,17 +1,36 @@
 from abc import ABC, abstractmethod
 from signals.Signal import Signal
 
-class SignalPeriodic(Signal, ABC):
 
-    def __init__(self,  sample_rate, start_on_sample, end_on_sample, max_amplitude, min_frequency, max_frequency, frequency_variation = 'lin', direction = 'slow-fast'):
+class SignalChirp(Signal, ABC):
+
+    def __init__(self, sample_rate, start_on_sample, end_on_sample, max_amplitude, min_frequency, max_frequency,
+                 frequency_variation='lin', initial_frequency_change='slow-fast', start_on_min_frequency=True):
         super().__init__(sample_rate, start_on_sample, end_on_sample, max_amplitude)
         self.min_frequency = min_frequency
         self.max_frequency = max_frequency
-        self.frequency_variation = frequency_variation # lin, log, exp, hyp
-        self.direction = direction # slow-fast, fast-slow
+        self.frequency_variation = frequency_variation  # lin, log, exp, hyp
+        self.initial_frequency_change = initial_frequency_change  # slow-fast, fast-slow
+        self.start_on_min_frequency = start_on_min_frequency
+        self.current_frequency = 0
+        self.delta_frequency_lin = (self.max_frequency - self.min_frequency) / (self.end_on_sample - self.start_on_sample)
 
-    def generate_single_sample_in_the_loop(self, current_sample_time):
-        #TODO:
+        if start_on_min_frequency:
+            self.current_frequency = self.min_frequency
+        else:
+            self.current_frequency = self.max_frequency
+            if self.initial_frequency_change == 'slow-fast':
+                self.initial_frequency_change = 'fast-slow'
+            if self.initial_frequency_change == 'fast-slow':
+                self.initial_frequency_change = 'slow-fast'
+
+    def change_values_after_generating_a_sample(self, current_sample_time):
+        # TODO:
         # Calculate frequency change based on linear, logarithmic, exponential or hyperbolic functions
         # Calculate frequency based on direction ('slow-fast', 'fast-slow')
-        pass
+        if self.frequency_variation == 'lin':
+            self.current_frequency += self.delta_frequency_lin
+
+    def post_sample_generator(self):
+        if not self.start_on_min_frequency:
+            self.sample_values = self.sample_values[::-1]
